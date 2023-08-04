3e405044533e29d7cf4a5f4bc030892c666b9b25
Ebrahim <morkasebrahim3@gmail.com>
2023-08-04 17:55:31 +0530

    Questions and its category are coded

---
+++ questions.py
@@ -0,0 +1,24 @@
+
+class Questions:
+    # Creating the list that will hold all the questions under different types such as history, geography and many more
+    list_of_questions = [
+        {"Indian History":{
+            {"question": "The Battle of Plassey was fought in",
+             "options":["1757", "1782", "1748", "1764"],
+             "answer": "1757"},
+            {"question": "The territory of Porus who offered strong resistance to Alexander was situated between the rivers of",
+             "options":["Sutlej and Beas", "Jhelum and Chenab", "Ravi and Chenab", "Ganga and Yamuna"],
+             "answer": "Jhelum and Chenab"}
+            }
+        },
+        {"Indian Geography":{
+            {"question": "The Paithan (Jayakwadi) Hydro-electric project, completed with the help of Japan, is on the river",
+             "options": ["Ganga", "Cauvery", "narmada", "godavari"],
+             "answer": "godavari"},
+            {"question": "The percentage of irrigated land in India is about",
+             "options": ["45", "65", "35"," 25"],
+             "answer": "35"}
+        }}
+    ]
+    # Creating the list that will hold the categories of the quiz that is "Indian History", "Indian Geography  and many more
+    list_of_category = ["Indian History", "Indian Geography"]
